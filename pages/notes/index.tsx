234e844257ccb294664ead41e922812d84e62545
Devin Sharpe <devinrsharpe@gmail.com>
2023-01-16 16:08:12 -0500

    feat: add notes page

---
+++ pages/notes/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import React from "react";
+
+const NotesIndexPage: NextPage = () => {
+  return <main className="page-container">hello world</main>;
+};
+
+export default NotesIndexPage;
